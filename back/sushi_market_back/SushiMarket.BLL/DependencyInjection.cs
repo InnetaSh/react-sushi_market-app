5bd75e8644dd99f5c5691c3dfddfccdb6e1e5fbb
Інна Шпонька <inneta3@gmail.com>
2026-08-26 19:13:08 +0300

    add validatorBehavior, validators for DTOs

--- back/sushi_market_back/SushiMarket.BLL/DependencyInjection.cs
+++ back/sushi_market_back/SushiMarket.BLL/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+﻿using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using SushiMarket.BLL.MediatR.Behaviors;
 
 namespace SushiMarket.BLL
 {
@@ -8,7 +10,13 @@ namespace SushiMarket.BLL
     {
         public static IServiceCollection AddBll(this IServiceCollection services)
         {
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             return services;
         }
---
+++ back/sushi_market_back/SushiMarket.BLL/Validators/Auth/LoginDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using SushiMarket.BLL.DTOs.Auth;
+using SushiMarket.BLL.Resources;
+
+namespace SushiMarket.BLL.Validators.Auth
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .ValidEmail(
+                    maxLength: 100,
+                    requiredMessage: ErrorMessages.EmailRequired,
+                    formatMessage: ErrorMessages.EmailInvalidFormat,
+                    lengthMessage: ErrorMessages.EmailMaxLength
+                );
+
+            RuleFor(x => x.Password)
+                .ValidPassword(
+                    minLength: 6,
+                    maxLength: 50,
+                    requiredMessage: ErrorMessages.PasswordRequired,
+                    minLengthMessage: ErrorMessages.PasswordMinLength,
+                    maxLengthMessage: ErrorMessages.PasswordMaxLength
+                );
+        }
+    }
+}
